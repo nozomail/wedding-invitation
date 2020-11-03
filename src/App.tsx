40f945ce6fc22578d024be3bd74e3a254ee4fda4
nozomail <nozomi.mail@icloud.com>
2020-11-03 23:42:50 +1300

    Add global styles

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './App.scss';
-
-export function App(): JSX.Element {
-  return <div className="App">App Loaded!</div>;
-}
---
+++ src/containers/App/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export function App(): JSX.Element {
+  return (
+    <div className="App">
+      App Loaded!
+    </div>
+  );
+}
