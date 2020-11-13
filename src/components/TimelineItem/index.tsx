f729d8e11e7688b184a82e5182f1b4c6fe70ea61
nozomail <nozomi.mail@icloud.com>
2020-11-14 00:02:44 +1300

    Add TimelineItem component

---
+++ src/components/TimelineItem/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.scss';
+
+import { HelperText } from '@components/HelperText';
+
+type Props = {
+  time: string;
+  items: {
+    name: string;
+    icon: string;
+    helper?: string;
+  }[];
+};
+
+export function TimelineItem({ time, items }: Props): JSX.Element {
+  return (
+    <div className="TimelineItem">
+      <div className="TimelineItem_head">{time}</div>
+      <div className="TimelineItem_body">
+        {items.map((item) => (
+          <>
+            <div className={`TimelineItem_item -${item.icon}`}>{item.name}</div>
+            {item.helper && <HelperText>{item.helper}</HelperText>}
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
