be4165021b1c8af1325642c5ecf42fdaeb2e5841
nozomail <nozomi.mail@icloud.com>
2020-11-14 00:01:53 +1300

    Use alias to import scss

--- src/containers/Venue/index.tsx
+++ src/containers/Venue/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/global.scss';
+import '@styles/global.scss';
 
 import { Card } from '@components/Card';
 import { Section } from '@components/Section';
