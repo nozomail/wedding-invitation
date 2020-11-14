0b265e3821ec5644dfad10b34cd56e5078f56122
nozomail <nozomi.mail@icloud.com>
2020-11-14 13:31:51 +1300

    Add xl size and color options to Block component

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -2,11 +2,19 @@ import React, { ReactNode } from 'react';
 import './style.scss';
 
 type Props = {
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | 'xlarge';
   type?: 'around' | 'vertical' | 'horizontal' | 'top' | 'bottom' | 'left' | 'right';
+  isColor?: boolean;
   children: ReactNode;
 };
 
-export function Block({ size = 'medium', type = 'around', children }: Props): JSX.Element {
-  return <div className={`Block -${type} -${size}`}>{children}</div>;
+export function Block({
+  type = 'around',
+  size = 'medium',
+  isColor = false,
+  children,
+}: Props): JSX.Element {
+  const color = isColor ? ' -color' : '';
+
+  return <div className={`Block -${type} -${size}${color}`}>{children}</div>;
 }
