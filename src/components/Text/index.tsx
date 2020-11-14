c2853c805613c95f38a15f30d44e5ed7787dd6fb
nozomail <nozomi.mail@icloud.com>
2020-11-14 13:32:12 +1300

    Add centering option to Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,9 +2,12 @@ import React, { ReactNode } from 'react';
 import './style.scss';
 
 type Props = {
+  isCenter?: boolean;
   children: ReactNode;
 };
 
-export function Text({ children }: Props): JSX.Element {
-  return <p className="Text">{children}</p>;
+export function Text({ isCenter = false, children }: Props): JSX.Element {
+  const textStyle = isCenter ? ' -center' : '';
+
+  return <p className={`Text${textStyle}`}>{children}</p>;
 }
