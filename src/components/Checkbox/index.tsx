db44f7d7f226930bde734895309f38a750e57390
nozomail <nozomi.mail@icloud.com>
2020-12-13 19:20:06 +1100

    Add Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+
+type Props = {
+  name: string;
+  label: string;
+  checked: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+export function Checkbox({ label, ...rest }: Props): JSX.Element {
+  return (
+    <label className="Checkbox">
+      <input type="checkbox" {...rest} />
+      <span className="Checkbox_label">{label}</span>
+    </label>
+  );
+}
