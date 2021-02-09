729f9a44aef46f5a63d136d3c1aa42488c7a26df
nozomail <nozomi.mail@icloud.com>
2021-02-09 13:27:47 +1100

    Add postcss

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.(svg|png|jpg|gif)$/i,
